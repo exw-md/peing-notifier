74e2e6320b322ff1aec564a367f0f2f2f453dd1a
Takuma HANATANI <mail@potato4d.me>
2023-03-16 01:37:46 +0900

    chore: modulize type

--- index.ts
+++ index.ts
@@ -8,9 +8,9 @@ import {
   doc,
   DocumentReference,
   getDoc,
-  getDocs,
   setDoc,
 } from '@firebase/firestore';
+import { Question } from './type';
 
 const environments: { [key: string]: string } = {
   APP_FIREBASE_COLLECTION_ID: `${process.env.APP_FIREBASE_COLLECTION_ID}`,
@@ -25,39 +25,6 @@ const app = initializeApp({
 });
 
 const firestore = getFirestore(app);
-
-type Question = {
-  id: number;
-  get_user_id: number;
-  body: string;
-  enum_status: string;
-  uuid_hash: string;
-  eye_catch: {
-    url: string;
-  };
-  locale: string;
-  country: null;
-  hope_answer_type: string;
-  make_item_url: string;
-  created_at: Date;
-  updated_at: Date;
-  time: string;
-  newer: boolean;
-  hope_video: boolean;
-  hope_audio: boolean;
-  item_url: string;
-  answer_url: string;
-  unread: boolean;
-  read: boolean;
-  dust: boolean;
-  eye_catch_url: string;
-  answer_body: null;
-  active_broadcast_banner: null;
-  is_popular_community_item: boolean;
-  is_liked: boolean;
-  is_receive_dm: boolean;
-  is_choose_question: boolean;
-};
 
 const iftttApiClient = axios.create({
   baseURL: 'https://maker.ifttt.com',
---
+++ type.ts
@@ -0,0 +1,32 @@
+export type Question = {
+  id: number;
+  get_user_id: number;
+  body: string;
+  enum_status: string;
+  uuid_hash: string;
+  eye_catch: {
+    url: string;
+  };
+  locale: string;
+  country: null;
+  hope_answer_type: string;
+  make_item_url: string;
+  created_at: Date;
+  updated_at: Date;
+  time: string;
+  newer: boolean;
+  hope_video: boolean;
+  hope_audio: boolean;
+  item_url: string;
+  answer_url: string;
+  unread: boolean;
+  read: boolean;
+  dust: boolean;
+  eye_catch_url: string;
+  answer_body: null;
+  active_broadcast_banner: null;
+  is_popular_community_item: boolean;
+  is_liked: boolean;
+  is_receive_dm: boolean;
+  is_choose_question: boolean;
+};
